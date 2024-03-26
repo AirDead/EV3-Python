bccbd09449143aac774518db52bf65490ca94e9b
cosmodev <cosmodev.orders@gmail.com>
2024-03-26 18:31:00 +0300

    Remove virtualenv

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ print("Color:", color_sensor.color)
 print("Reflected light intensity:", color_sensor.reflected_light_intensity)
 print("RGB:", color_sensor.rgb)
 
-# Set the sensor to RGB mode
 color_sensor.mode = ColorSensor.MODE_RGB_RAW
 
-# Read RGB values in RAW mode
 rgb_values_raw = color_sensor.raw
 print("RGB values (RAW):", rgb_values_raw)
