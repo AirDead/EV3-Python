268542b00cdd02e1e257daded9b7db1c587ae241
cosmodev <cosmodev.orders@gmail.com>
2024-03-26 18:12:23 +0300

    Color sensor test

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python3
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
-from ev3dev2.sensor import INPUT_1, ColorSensor
-from ev3dev2.sensor.lego import TouchSensor
-from ev3dev2.led import Leds
+from ev3dev2.sensor.lego import ColorSensor
+from time import sleep
 
+color_sensor = ColorSensor()
 
-color = ColorSensor(INPUT_1)
+print("Color:", color_sensor.color)
+print("Reflected light intensity:", color_sensor.reflected_light_intensity)
+print("RGB:", color_sensor.rgb)
 
-print(color.color)
-
-print(color.reflected_light_intensity)
-
-print(color.rgb)
-
-# Установить датчик в режим RGB
+# Set the sensor to RGB mode
 color_sensor.mode = ColorSensor.MODE_RGB_RAW
 
-# Прочитать RGB значения в режиме RAW
+# Read RGB values in RAW mode
 rgb_values_raw = color_sensor.raw
-print("RGB значения (RAW):", rgb_values_raw)
-
-
+print("RGB values (RAW):", rgb_values_raw)
