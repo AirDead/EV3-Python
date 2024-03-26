b0e7a30caaa60b31d15e86bd966415b259efc2e9
cosmodev <cosmodev.orders@gmail.com>
2024-03-26 20:41:20 +0300

    Core edit to provide current port

--- Core/Core.py
+++ Core/Core.py
@@ -4,20 +4,23 @@ from ev3dev2.sensor.lego import ColorSensor
 
 class EV3Controller:
     def __init__(self, motor_ports=None, color_sensor_ports=None):
-        self.motors = [LargeMotor(port) for port in motor_ports if port and port.isalpha()]
-        self.color_sensors = [ColorSensor(port) for port in color_sensor_ports if port and port.isdigit()]
+        self.motors = {port: LargeMotor(port) for port in motor_ports if port and port.isalpha()}
+        self.color_sensors = {port: ColorSensor(port) for port in color_sensor_ports if port and port.isdigit()}
 
-    def move_forward(self, speed=50):
-        for motor in self.motors:
-            motor.on(speed=speed)
+    def move_motor_forward(self, motor_port, speed=50):
+        if motor_port in self.motors:
+            self.motors[motor_port].on(speed=speed)
 
-    def move_backward(self, speed=50):
-        for motor in self.motors:
-            motor.on(speed=-speed)
+    def move_motor_backward(self, motor_port, speed=50):
+        if motor_port in self.motors:
+            self.motors[motor_port].on(speed=-speed)
 
-    def stop_motors(self):
-        for motor in self.motors:
-            motor.off()
+    def stop_motor(self, motor_port):
+        if motor_port in self.motors:
+            self.motors[motor_port].off()
 
-    def get_colors(self):
-        return [sensor.color for sensor in self.color_sensors]
+    def get_color(self, color_sensor_port):
+        if color_sensor_port in self.color_sensors:
+            return self.color_sensors[color_sensor_port].color
+        else:
+            return None
